a846945195fd2f571ddf2f87a4c1c80e0b377043
lorenzocerrone <lorenzo.cerrone@gmail.com>
2020-11-03 12:23:50 +0100

    add multiprocessing

--- plantsegtools/meshes/meshes.py
+++ plantsegtools/meshes/meshes.py
@@ -1,11 +1,13 @@
 import concurrent.futures
-
+import multiprocessing as mp
 import numpy as np
 import trimesh
 from skimage import measure
 
 from plantsegtools.utils.clean_segmentation import relabel_segmentation
 from plantsegtools.utils.io import smart_load
+import tqdm
+from functools import partial
 
 
 def _filter_2d_masks(mask):
@@ -34,14 +36,17 @@ def mask2mesh(mask, voxel_size=(1.0, 1.0, 1.0), level=0, step_size=2, preprocess
     return mesh
 
 
-def masks_generator(segmentation, min_size=50, max_size=np.inf, relabel_cc=True):
-    # relabel segmentation to avoid issues with not connected components
-    segmentation = relabel_segmentation(segmentation) if relabel_cc else segmentation
+def masks_generator(segmentation, min_size=50, max_size=np.inf, idx_list=None, relabel_cc=True, remove_labels=(0, )):
+    # relabel segmentation to avoid issues with not connected components (only if no idxlist is provided)
+    segmentation = relabel_segmentation(segmentation) if relabel_cc and idx_list is None else segmentation
 
-    # Filter small and large segments
-    counts = np.bincount(segmentation.ravel())
-    objects_list = list(filter(lambda x: x[1] != 0 and min_size < x[1] < max_size, enumerate(counts)))
-    filtered_idx = np.asarray(objects_list)[:, 0]
+    if idx_list is None:
+        filtered_idx = idx_list
+    else:
+        # Filter small and large segments
+        counts = np.bincount(segmentation.ravel())
+        objects_list = list(filter(lambda x: min_size < x[1] < max_size and x[0] not in remove_labels, enumerate(counts)))
+        filtered_idx = np.asarray(objects_list)[:, 0]
 
     # yield idx and masks
     for idx in filtered_idx:
@@ -51,62 +56,61 @@ def masks_generator(segmentation, min_size=50, max_size=np.inf, relabel_cc=True)
             yield idx, mask
 
 
-def seg2mesh(stack_path,
-             save_path='/home/lcerrone/testply/',
-             h5_key='label',
-             preprocessing=None,
-             postprocessing=None,
-             min_size=50,
-             max_size=np.inf,
-             relabel_cc=True
-             ):
+def _seg2mesh(idx, mask,
+              base_name='test_stack',
+              base_path='./test-ply/',
+              voxel_size=(1.0, 1.0, 1.0),
+              preprocessing=None,
+              postprocessing=None):
 
-    def _seg2mesh(args):
-        idx, mask = args
-        mesh = mask2mesh(mask,
-                         voxel_size=voxel_size,
-                         level=0,
-                         preprocessing=preprocessing,
-                         postprocessing=postprocessing)
-
-        create_ply(f"{save_path}/test{idx}.ply", mesh)
-
-    stack, voxel_size = smart_load(stack_path, h5_key)
-    [_seg2mesh(i) for i in masks_generator(stack, min_size=min_size, max_size=max_size, relabel_cc=relabel_cc)]
-
-
-def _seg2mesh_mp(args):
-    idx, mask = args
-    save_path = '/home/lcerrone/testply/'
+    """basic seg2mesh loop"""
     mesh = mask2mesh(mask,
-                     voxel_size=(1.0, 1.0, 1.0),
+                     voxel_size=voxel_size,
                      level=0,
-                     preprocessing=None,
-                     postprocessing=None)
-    create_ply(f"{save_path}/test{idx}.ply", mesh)
+                     preprocessing=preprocessing,
+                     postprocessing=postprocessing)
+
+    create_ply(f"{base_path}/{base_name}_{idx:05d}.ply", mesh)
 
 
 def seg2mesh_mp(stack_path,
-                save_path='/home/lcerrone/testply/',
+                base_name='test_stack',
+                base_path='./test-ply/',
+                n_process=2,
                 h5_key='label',
+                voxel_size=None,
                 preprocessing=None,
                 postprocessing=None,
                 min_size=50,
                 max_size=np.inf,
-                relabel_cc=True
+                idx_list=None,
+                relabel_cc=False
                 ):
 
-    stack, voxel_size = smart_load(stack_path, h5_key)
-    list_idx = list(masks_generator(stack, min_size=min_size, max_size=max_size, relabel_cc=relabel_cc))
-    with concurrent.futures.ProcessPoolExecutor(2) as executor:
-        executor.map(_seg2mesh_mp, list_idx)
+    stack, _voxel_size = smart_load(stack_path, h5_key)
+    voxel_size = _voxel_size if voxel_size is None else (1.0, 1.0, 1.0)
+    partial_seg2mesh = partial(_seg2mesh,
+                               base_name=base_name,
+                               base_path=base_path,
+                               voxel_size=voxel_size,
+                               preprocessing=preprocessing,
+                               postprocessing=postprocessing)
+    _mask_generator = masks_generator(stack,
+                                      min_size=min_size,
+                                      max_size=max_size,
+                                      idx_list=idx_list,
+                                      relabel_cc=relabel_cc)
+    if n_process > 1:
+        list_idx = list(_mask_generator)
+        with mp.Pool(processes=n_process) as executor:
+            executor.starmap(partial_seg2mesh, list_idx)
+    else:
+        [partial_seg2mesh(mask, idx) for mask, idx in tqdm.tqdm(_mask_generator)]
 
 
 if __name__ == '__main__':
     import time
     timer = time.time()
     sample_path = "/home/lcerrone/datasets/small_samples/sample_ovules.h5"
-    #seg2mesh(sample_path, max_size=100000)
-    seg2mesh_mp(sample_path, max_size=100000)
-
-    print(time.time() - timer)
+    seg2mesh_mp(sample_path, max_size=10000, nprocess=2)
+    print("global timer: ", time.time() - timer)
